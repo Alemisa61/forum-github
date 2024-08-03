65f7fd41d63a9f0efe08e8a15fd166eeb9782398
Alemisa61 <161025222+Alemisa61@users.noreply.github.com>
2024-08-03 12:33:03 +0300

    hey guys I added Login functionality under user comtroller file

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -27,6 +27,40 @@ const login = async (req, res) => {
   // Generate JWT token
 
   // Send successful response with token
+
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(401).json({ error: "Email and password are required" });
+  }
+
+  try {
+    const [user] = await dbConnection.query(
+      "SELECT userid, username, password FROM users WHERE email = ?",
+      [email]
+    );
+
+    if (user.length === 0) {
+      return res.status(401).json({ error: "Invalid credentials" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user[0].password);
+    if (isMatch) {
+      const token = jwt.sign(
+        { username: user[0].username, userid: user[0].userid },
+        process.env.JWT_SECRET
+      );
+      res.status(HttpStatus.OK).json({
+        message: "User login successful",
+        token: token,
+        username: user[0].username,
+      });
+    } else {
+      return res.status(401).json({ error: "Invalid password" });
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    return res.status(500).json({ error: "Server Error" });
+  }
 };
 
 const checkUser = async (req, res) => {
