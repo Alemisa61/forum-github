9e8d4c1acca04b75ba304ac292d89928e1538fcd
11339844 <sol339844@gmail.com>
2024-08-04 22:51:53 -0400

    Add post answer code

--- backend/routes/answerRoute.js
+++ backend/routes/answerRoute.js
@@ -1,16 +1,16 @@
-const router = require("express").Router();
+const express = require("express");
+const router = express.Router();
 
 //answer controllers
-const { getAnswers, postAnswer } = require("../controllers/answerController");
+const { postAnswer } = require("../controllers/answerController");
 
 //auth middleware
-const {authMiddleware} = require("../middlewares/authMiddleware");
+const authMiddleware = require("../middlewares/authMiddleware");
 
 //Get answers for a single question route
-router.get("/:question_id", authMiddleware, getAnswers);
+// router.get("/:question_id", authMiddleware, getAnswers);
 
 //Post answer for a single question route
-router.post("/:question_id",authMiddleware, postAnswer);
-
+router.post("/:question_id", authMiddleware, postAnswer);
 
 module.exports = router;
