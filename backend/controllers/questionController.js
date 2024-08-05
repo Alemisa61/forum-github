3febcae4ff5c1cc6efe6dcbe9dd2bbeae9a47fc2
Natti2024 <tattigashu@yahoo.com>
2024-08-04 20:00:13 -0700

    Task 8 & 13 added!

--- backend/controllers/questionController.js
+++ backend/controllers/questionController.js
@@ -29,7 +29,28 @@ const postQuestion = async (req, res) => {
   }
 };
 
-const getAllQuestions = async (req, res) => {};
+const getAllQuestions = async (req, res) => {
+  try {
+    // Perform an SQL query to select specific columns from the 'questions' table,
+    // joining it with the 'users' table based on matching 'user_name' and 'username'.
+    // The results are ordered by 'id' in descending order.
+    // The result of the query is destructured into the 'questions' variable.
+    const [questions] = await dbConnection.query(
+      "SELECT title,description,question_id,user_name FROM questions JOIN users ON users.username = questions.user_name ORDER BY id DESC"
+    );
+    // Send a response with HTTP status 200 (OK) and the queried data as JSON.
+    // The data is wrapped in an object with a 'questions' property.
+    return res.status(StatusCodes.OK).json({ questions });
+  } catch (error) {
+    console.log(error.message);
+    // Send a response with HTTP status 500 (Internal Server Error) indicating that an unexpected error occurred.
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      error: "Internal Server Error",
+      message: "An unexpected error occurred",
+    });
+  }
+};
+
 
 const getSingleQuestion = async (req, res) => {};
 module.exports = { getAllQuestions, getSingleQuestion, postQuestion };
